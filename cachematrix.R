a427ea1723cb2e4c168fd7b139e5743edb266f2d
Krystian Zbikowski <k_zbik@wp.pl>
2015-04-20 21:10:59 +0200

    Project2 asignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions below calculate, store and return inverse of the square matrix
 
-## Write a short comment describing this function
+## makeCacheMAtrix function stores given matrix in enclosed environment cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##initialize m variable
+        m <- NULL
+        ##function storing input matrix in enclosing envirenment
+        setmatrix <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        ##function getting input matrix from cache
+        getmatrix <- function() x
+        ##function calculating inverse matrix
+        setinverse <- function(solve) m <<- solve
+        ##function getting m vaariable value fro enclosing environment
+        getinverse <- function() m
+        ##Output of makeCacheMatrix - list
+        list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calculate, store, get and return inverse matrix 
+## of a given matrix from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##get matrix inverse from cache and storing it in m variable
+        m <- x$getinverse()
+        ##check if matrix inverse is stored in cache (m variable is not null)
+        if(!is.null(m)) {
+                ##inform user that matrix is retured from cache
+                message("getting cached inverse matrix")
+                ##return stored inverse matrix 
+                return(m)
+        }
+        ##Getting matrix from cache
+        data <- x$getmatrix()
+        ##calculating inverse matrix and storing the inverse matrix in m variable
+        m <- solve(data, ...)
+        ##putting m vaiable into cache
+        x$setinverse(m)
+        ##returning m variable - matrix inverse
+        m     
 }
